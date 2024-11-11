cb3552c29d20207deedda86371c263e0e671eb58
Patrick Heneise <patrick@zentered.co>
2024-11-11 10:54:01 -0700

    chore: cf deployment

--- app.config.js
+++ app.config.js
@@ -3,16 +3,16 @@ import tailwindcss from 'tailwindcss'
 import { config } from 'vinxi/plugins/config'
 
 export default defineConfig({
-  appRoot: './src',
+  appRoot: 'src',
   islands: false,
   server: {
     preset: 'cloudflare_module',
     rollupConfig: {
       external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks']
+    },
+    prerender: {
+      crawlLinks: true
     }
-    // prerender: {
-    //   crawlLinks: true
-    // }
   },
   vite: {
     optimizeDeps: {
